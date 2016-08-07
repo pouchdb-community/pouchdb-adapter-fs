ad59216976185b23b34fa957d60b8040db60e080
Nolan Lawson <nolan.lawson@gmail.com>
2016-08-07 16:57:37 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var fsdown = require('fsdown');
+var CoreLevelPouch = require('pouchdb-adapter-leveldb-core');
+var extend = require('js-extend').extend;
+function LevelDownPouch(opts, callback) {
+  var _opts = extend({
+    db: fsdown,
+    migrate: false
+  }, opts);
+  CoreLevelPouch.call(this, _opts, callback);
+}
+// overrides for normal LevelDB behavior on Node
+LevelDownPouch.valid = function () {
+  return true;
+};
+
+LevelDownPouch.use_prefix = false;
+module.exports = function (PouchDB) {
+  PouchDB.adapter('fsdown', LevelDownPouch, true);
+};
