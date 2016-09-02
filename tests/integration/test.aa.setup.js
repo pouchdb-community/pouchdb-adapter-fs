d527b3205aabb814a8041fb6b6234ecad513ec7b
Gregor <gregor@martynus.net>
2016-09-01 20:30:43 -0700

    test: remove browser tests

--- tests/integration/test.aa.setup.js
+++ tests/integration/test.aa.setup.js
@@ -15,11 +15,4 @@ describe('DB Setup', function () {
     PouchDB.version.should.be.a('string');
     PouchDB.version.should.match(/\d+\.\d+\.\d+/);
   });
-
-  if (typeof process !== 'undefined' && !process.browser) {
-    it('PouchDB version matches package.json', function () {
-      var pkg = require('../../packages/pouchdb/package.json');
-      PouchDB.version.should.equal(pkg.version);
-    });
-  }
 });
